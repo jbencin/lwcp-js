0a67a1093303582557995c7e417780d6be56a678
Jeff Bencin <jeff.bencin@gmail.com>
2018-02-21 16:59:50 -0500

    Inital commit of typescript code in separate project

---
+++ src/lwcp.spec.ts
@@ -0,0 +1,221 @@
+import { LWCP } from "./lwcp"
+
+describe('LWCP Basic Tests', () => {
+    it('Load LWCP test spec file', () => {
+        expect(true).toBe(true);
+    });
+
+    it('Import LWCP module', () => {
+        expect(LWCP).toBeTruthy();
+    });
+})
+
+describe('LWCP.Message', () => {
+    describe('Build a message with Legal Values', () => {
+        let test = {
+            cmd: 'call',
+            objs: [
+                { name:  'studio',  id:    null, desc:    '     null id' },
+                { name:    'show',  id: 'room1', desc:    '    mixed id' },
+                { name:    'line',  id:     '1', desc:    '  numeric id' },
+            ],
+            props: [
+                { name:  'number', val:   '101', desc:    'string value' }
+            ],
+            sysProps: [
+                { name:    '$ack', val:  'true', desc:    'string value' }
+            ]
+        }
+
+        let msg  = new LWCP.Message(test.cmd);
+
+        it(`Create Message '${test.cmd}'`, () => {
+            expect(Boolean(msg)).toBe(true);
+            expect(msg.op).toMatch(test.cmd);
+        });
+
+        describe('Message.addObj()', () => {
+            for (let i in test.objs) {
+                let obj = test.objs[i];
+                if (obj.id) {
+                    it(`Test (${obj.desc}): ${obj.name}#${obj.id}`, () => {
+                        msg.addObj(obj.name, obj.id);
+                        expect(msg.objs[i].name).toBe(obj.name);
+                        expect(msg.objs[i].id).toBe(obj.id);
+                    });
+                } else {
+                    it(`Test (${obj.desc}): ${obj.name}`, () => {
+                        msg.addObj(obj.name);
+                        expect(msg.objs[i].name).toBe(obj.name);
+                    });
+                }
+            }
+        });
+
+        describe('Message.addProp()', () => {
+            for (let i in test.props) {
+                let prop = test.props[i];
+                if (prop.val) {
+                    it(`Test (${prop.desc}): ${prop.name}=${prop.val}`, () => {
+                        msg.addProp(prop.name, prop.val);
+                        expect(msg.props[i].name).toBe(prop.name);
+                        expect(msg.props[i].val).toMatch(prop.val);
+                    });
+                } else {
+                    it(`Test (${prop.desc}): ${prop.name}`, () => {
+                        msg.addProp(prop.name);
+                        expect(msg.props[i].name).toBe(prop.name);
+                    });
+                }
+            }
+        });
+
+        describe('Message.addProp(): sysprops', () => {
+            for (let i in test.sysProps) {
+                let sysProp = test.sysProps[i];
+                if (sysProp.val) {
+                    it(`Test (${sysProp.desc}): ${sysProp.name}=${sysProp.val}`, () => {
+                        msg.addProp(sysProp.name, sysProp.val);
+                        expect(msg.sysProps[i].name).toBe(sysProp.name);
+                        expect(msg.sysProps[i].val).toMatch(sysProp.val);
+                    });
+                } else {
+                    it(`Test (${sysProp.desc}): ${sysProp.name}`, () => {
+                        msg.addProp(sysProp.name);
+                        expect(msg.sysProps[i].name).toBe(sysProp.name);
+                    });
+                }
+            }
+        });
+    });
+})
+
+describe('LWCP.Parser', () => {
+
+    // Compare javascript value to LWCP.Value. Necessary to handle arrays
+    let compareValue = (v: LWCP.Value, a: any) => {
+        if (Array.isArray(v.val)) {
+            expect(v.val.length).toBe(a.length);
+            for (let j in v.val) {
+                compareValue(v.val[j], a[j]);
+            }
+        } else {
+            expect(v.val).toBe(a);
+        }
+    }
+
+    it(`Create Parser`, () => {
+        let parser = new LWCP.Parser;
+        expect(Boolean(parser)).toBe(true);
+    })
+
+    describe('Parser.parseValue()', () => {
+        let parser = new LWCP.Parser;
+        // Text 'in' should parse to value 'val'
+        let test = [
+            { in:                                '100', val:               100, desc: '            positive integer'},
+            { in:                              '+5555', val:              5555, desc: '   explicit positive integer'},
+            { in:                               '-100', val:              -100, desc: '            negative integer'},
+            { in:                               '0xFF', val:               255, desc: '                 hex integer'},
+            { in:                            '    100', val:               100, desc: '  leading whitespace integer'},
+            { in:                             '3.1415', val:            3.1415, desc: '     positive floating point'},
+            { in:                            '+3.1415', val:            3.1415, desc: 'exp. positive floating point'},
+            { in:                            '-3.1415', val:           -3.1415, desc: '     negative floating point'},
+            { in:                                 '[]', val:                [], desc: '                 empty array'},
+            { in:                                '[1]', val:               [1], desc: '                simple array'},
+            { in:                  '["str",  2.5, ""]', val:  ['str', 2.5, ''], desc: '                 mixed array'},
+            { in:                   '[[], [1,2], "a"]', val:  [[], [1,2], 'a'], desc: '                    2d array'},
+            { in:                    '[ [ 1 ] , [ ] ]', val:         [[1], []], desc: '         whitespace in array'},
+            { in:                           'ENUM_999', val:        'ENUM_999', desc: '                 enum string'},
+            { in:                    '"hello, world!"', val:   'hello, world!', desc: '        double quoted string'},
+            { in:                    "'hello, world!'", val:   'hello, world!', desc: '        single quoted string'},
+            { in:   '%BeginEncap% "Hello", %EndEncap%', val:      ' "Hello", ', desc: '                encap string'},
+            { in:                                 '""', val:                '', desc: '   double quoted null string'},
+            { in:                                 "''", val:                '', desc: '   single quoted null string'},
+            { in:             '%BeginEncap%%EndEncap%', val:                '', desc: '           encap null string'},
+            { in:                                '!!!', val:              null, desc: '               illegal input'},
+            { in:                                 '()', val:              null, desc: '               illegal input'},
+            { in:                                  '"', val:              null, desc: '               illegal input'},
+            { in:                                  "'", val:              null, desc: '               illegal input'},
+            { in:                                  '%', val:              null, desc: '               illegal input'},
+            { in:            '%BeginEncap% Not Closed', val:              null, desc: '               illegal input'},
+            { in:                         '%EndEncap%', val:              null, desc: '               illegal input'},
+            { in:                                 '][', val:              null, desc: '               illegal input'},
+            { in:                                   '', val:              null, desc: '               illegal input'},
+            { in:                              '     ', val:              null, desc: '               illegal input'},
+        ];
+
+        for (let i in test) {
+            let e = test[i];
+            it(`Test (${e.desc}): ${e.in}`, () => {
+                let v = new LWCP.Value();
+                parser.parseValue(v, e.in);
+                compareValue(v, e.val);
+            });
+        }
+    })
+    describe('Parser.parseMessage()', () => {
+        let parser = new LWCP.Parser;
+        let str = '  call   studio#room700.line#3    number="555-1234" , hybrid = false  $ack ';
+        it(`Test: ${str}`, () => {
+            parser.parseMessage(str);
+            let msg = parser.getMessage();
+            expect(msg).toBeTruthy();
+            expect(msg.op).toBe('call');
+            expect(msg.objs[0].name).toBe('studio');
+            expect(msg.objs[0].id).toBe('room700');
+            expect(msg.objs[1].name).toBe('line');
+            expect(msg.objs[1].id).toBe('3');
+            expect(msg.props[0].name).toBe('number');
+            compareValue(msg.props[0].val, '555-1234');
+            expect(msg.props[1].name).toBe('hybrid');
+            compareValue(msg.props[1].val, 'false');
+        })
+    })
+    describe('Parser.getLineFromBuffer()', () => {
+        let parser = new LWCP.Parser;
+        let str = `drop studio.line#1
+                   drop studio.line#2 data=%BeginEncap%  ""'',, %EndEncap%    data2=%BeginEncap%%EndEncap%
+                   drop studio.line#3 data=%BeginEncap%
+%EndEncap%
+                   drop studio.line#4
+                   `;
+        it(`Test: ${str}`, () => {
+            parser.addDataToBuffer(str);
+            let line = parser.getLineFromBuffer().trim();
+            expect(line).toBe(`drop studio.line#1`);
+            line = parser.getLineFromBuffer().trim();
+            expect(line).toBe(`drop studio.line#2 data=%BeginEncap%  ""'',, %EndEncap%    data2=%BeginEncap%%EndEncap%`);
+            line = parser.getLineFromBuffer().trim();
+            expect(line).toBe(`drop studio.line#3 data=%BeginEncap%
+%EndEncap%`);
+            line = parser.getLineFromBuffer().trim();
+            expect(line).toBe(`drop studio.line#4`);
+        })
+    })
+    describe('Parser.parse()', () => {
+        let parser = new LWCP.Parser;
+        let str = `  call   studio#room700.line#3    number="555-1234" , hybrid = false  $ack
+                     drop   studio.line#6
+                     `;
+        it(`Test: ${str}`, () => {
+            parser.parse(str);
+            let msg = parser.getMessage();
+            expect(msg).toBeTruthy();
+            expect(msg.op).toBe('call');
+            expect(msg.objs[0].name).toBe('studio');
+            expect(msg.objs[0].id).toBe('room700');
+            expect(msg.objs[1].name).toBe('line');
+            expect(msg.objs[1].id).toBe('3');
+            expect(msg.props[0].name).toBe('number');
+            compareValue(msg.props[0].val, '555-1234');
+            expect(msg.props[1].name).toBe('hybrid');
+            compareValue(msg.props[1].val, 'false');
+            msg = parser.getMessage();
+            expect(msg).toBeTruthy();
+            expect(msg.objs[0].name).toBe('studio');
+            expect(msg.objs[1].name).toBe('line');
+            expect(msg.objs[1].id).toBe('6');
+        })
+    })
+})
---
+++ src/lwcp.ts
@@ -0,0 +1,496 @@
+export namespace LWCP {
+
+	enum Err {
+		NONE            = "No Error",
+		NOMSG           = "No Message",
+		IGNORED         = "Message Ignored",
+		DELAYMSG        = "Message Delayed",
+		NOTFOUND        = "Not Found",
+		NOTIMPLEMENTED  = "Not Implemented",
+		EXISTS          = "Already Exists",
+		READONLY        = "Read Only",
+		WRITEONLY       = "Write Only",
+		NOMEM           = "Out of Memory",
+		BADPTR          = "Bad Pointer",
+		BADAUTH         = "Unauthorized",
+		BADSTATE        = "Bad Internal State",
+		BADTYPE         = "Incorrect Type",
+		ENDSTREAM       = "End of Stream",
+		OTHER           = "Unknown Error",
+		MSG_INCOMPLETE  = "Message Incomplete",
+		MSG_NOID        = "Missing Identifier",
+		MSG_INVID       = "Invalid Identifier",
+		MSG_NOOP        = "Missing Operation",
+		MSG_INVOP       = "Invalid Operation",
+		MSG_NOOBJ       = "Missing Object",
+		MSG_INVOBJ      = "Invalid Object",
+		MSG_INVPROP     = "Invalid Property",
+		MSG_INVSPROP    = "Invalid System Property",
+		MSG_INVVAL      = "Invalid Value",
+		MSG_SYNTAX      = "Message Syntax Error",
+		MSG_OTHER       = "Unknown Message Error",
+	}
+
+	enum PropType {
+		PROP    = "PROP",
+		SYSPROP = "SYSPROP",
+	};
+
+	enum Type {
+		INVALID = "INVALID", // Invalid data type
+		NONE    = "NONE",    // Property with no value
+		NUMBER  = "NUMBER",  // Int or Float
+		STRING  = "STRING",  // ASCII string
+		ARRAY   = "ARRAY",   // Array of some other type
+		ENUM    = "ENUM",    // Enumerated value
+		ENCAP   = "ENCAP",   // Data enclosed by %BeginEncap% and %EndEncap%
+		OTHER   = "OTHER",   // Can use when building message for user formatted data
+	}
+
+	enum Encap {
+		BEGIN  = "%BeginEncap%", // This string signals start of encap data
+		END    = "%EndEncap%",   // This string signals end of encap data
+	}
+
+	//
+	const RGX_LWCP_NAME  = /^[a-z]\w*$/i;
+	const RGX_PROP_NAME  = /^\$?[a-z]\w*$/i; // Same as above with optional '$'
+	const RGX_OBJ_ID     = /^\w+$/;
+
+ 	// LWCP Object type. Can't use name 'Object' because that's already defined in Javascript
+	export class Obj {
+		name:  string;
+		id:    string;
+
+		constructor(name: string, id?: string) {
+			this.setName(name);
+			this.setID(id);
+		}
+
+		setName(name: string) : Obj {
+			if (!RGX_LWCP_NAME.test(name)) throw `Obj.setName(): Invalid argument '${name}'`;
+			this.name = name;
+			return this;
+		}
+
+		setID(id?: string) : Obj {
+			if (!Boolean(id)) {
+				this.id = null;
+			} else if (RGX_OBJ_ID.test(id)) {
+				this.id = id;
+			} else {
+				throw `Obj.setID(): Invalid argument '${id}'`;
+			}
+			return this;
+		}
+
+		toString() : string {
+			let str = this.name;
+			if (Boolean(this.id)) {
+				str += `#${this.id}`;
+			}
+			return str;
+		}
+	}
+
+ 	// LWCP Value
+	export class Value {
+		type:  Type;        // LWCP type, not Javascript type
+		val:   any;         // Value
+
+		constructor(val: any = null, type?: Type) {
+			this.setVal(val);
+			if (type) {
+				this.setType(type);
+			}
+		}
+
+		setVal(val: any = null) : Value {
+			// Note on null vs undefined:
+			//   null      / Type.NONE    = Used when property has no value
+			//   undefined / Type.INVALID = Used when trying to assign illegal value
+			if (val === null) {
+				this.type = Type.NONE;
+				this.val  = null;
+				return this;
+			}
+
+			switch (typeof(val)) {
+				case "number":
+					this.type = Type.NUMBER;
+					this.val  = val;
+					return this;
+				case "string":
+					this.type = Type.STRING;
+					this.val  = val;
+					return this;
+				case "boolean":
+					break;
+				case "object":
+					if (Array.isArray(val)) {
+						this.type = Type.ARRAY;
+						this.val  = val;
+						return this;
+					}
+					break;
+				case "function":
+					// I'm going to allow this, but for simplicity evalate on set
+					// Don't return another function from this or you'll get a stack overflow!!!
+					this.setVal(val());
+					return this;
+				case "undefined":
+					this.type = Type.INVALID;
+					this.val  = undefined;
+					return this;
+			}
+
+			// Default for anything we don't handle above
+			this.type = Type.OTHER;
+			this.val  = val;
+			return this;
+		}
+
+		setType(type: Type) : Value {
+			// Can only switch between string types
+			switch (this.type) {
+				case Type.ENCAP:
+				case Type.STRING:
+				case Type.ENUM:
+					this.type = type;
+					return this;
+			}
+			console.error(`Value.setVal(): Cannot set type of ${this.type} to ${type}`);
+			return this;
+		}
+
+		toString() : string {
+			switch (this.type) {
+				case Type.INVALID:
+				case Type.NONE:
+					return "";
+				case Type.NUMBER:
+				case Type.ENUM:
+				case Type.OTHER:
+ 					// Nothing special needs to be done, just use toString()
+					return this.val.toString();
+				case Type.ENCAP:
+ 					// Embed value encap tags
+					return `${Encap.BEGIN}${this.val.toString()}${Encap.END}`
+				case Type.STRING:
+ 					// Embed value in double qoutes
+					return `"${this.val.toString()}"`
+				case Type.ARRAY:
+					// Return comma separated list embedded in square brackets
+					let strArr = this.val.map((e: any) => e.toString());
+					return `[${strArr.join(',')}]`;
+			}
+		}
+	}
+
+ 	// LWCP Property type. Used for properties and system properties
+	export class Prop {
+		name: string;
+		val:  Value;
+		type: PropType;
+
+		constructor(name: string, ...args: any[]) {
+			this.setName(name);
+			this.setVal(...args);
+		}
+
+		setName(name: string) : Prop {
+			if (RGX_PROP_NAME.test(name)) {
+				this.type = (name[0] === '$') ? PropType.SYSPROP : PropType.PROP;
+			} else {
+ 				throw `Prop.setName(): Invalid argument '${name}'`;
+			}
+			this.name = name;
+			return this;
+		}
+
+		setVal(...args: any[]) : Prop {
+			if (args && args.length) {
+				let val = args.shift();
+				if (val instanceof Value) {
+					this.val = val;
+				} else {
+					this.val = new Value(val, ...args);
+				}
+			} else {
+				this.val = new Value();
+			}
+			return this;
+		}
+
+		toString() : string {
+			let str    = this.name;
+			let strVal = this.val.toString();
+			if (Boolean(strVal)) {
+				str += `=${strVal}`;
+			}
+			return str.trim();
+		}
+	}
+
+	export class Message {
+		op:       string;      // LWCP operation. Also determines if message is valid
+		objs:     Obj[];       // List of Objects/Selectors
+		props:    Prop[];      // LWCP Properties
+		sysProps: Prop[];      // LWCP System Properties
+
+		constructor(op: string) {
+			this.setOp(op);
+			this.objs     = [];
+			this.props    = [];
+			this.sysProps = [];
+		}
+
+		// DO NOT SET FIELDS DIRECTLY, USE add/set FUNCTIONS TO DO SO SAFELY!!!
+		// Also, all the add/set functions should return 'this' to allow chaining
+		setOp(op: string) : Message {
+			if (!RGX_LWCP_NAME.test(op)) throw `Message.setOp(): Invalid argument '${op}'`;
+			this.op = op;
+			return this;
+		}
+
+		addObj(obj: Obj | string, ...args: any[]) : Message {
+			// Can use either existing obj or make new obj from string+args
+			if ( !(obj instanceof Obj) ) {
+				obj = new Obj(obj, ...args);
+			}
+			this.objs.push(obj);
+			return this;
+		}
+
+		// Use same interface for adding props and sysprops so user doesn't have to think about it
+		addProp(prop: Prop | string, ...args: any[]) : Message {
+			// Can use either existing prop or make new prop from string+args
+			if ( !(prop instanceof Prop) ) {
+				prop = new Prop(prop, ...args);
+			}
+			switch (prop.type) {
+				case PropType.PROP:    this.props.push(prop);    break;
+				case PropType.SYSPROP: this.sysProps.push(prop); break;
+				default: throw `Message.addProp(): Invalid type '${prop.type}'`;
+			}
+			return this;
+		}
+
+		toString() : string {
+			// Write all 4 sections out as elements of an array
+			let strArr = [this.op];
+			strArr.push(this.objs.map    (e => e.toString()).join('.'));
+			strArr.push(this.props.map   (e => e.toString()).join(','));
+			strArr.push(this.sysProps.map(e => e.toString()).join(' '));
+
+			// Remove null strings and join array with spaces
+			return strArr.filter(e => Boolean(e)).join(' ');
+		}
+	}
+
+	export class Parser {
+		private buffer:   string;    // Buffer to be parsed
+		private messages: Message[]; // Parsed messages waiting in queue
+
+		constructor() {
+			this.buffer   = '';
+			this.messages = [];
+		}
+
+		// Parse text which may contain one or more messages
+		// THIS IS THE ONLY PARSE FUNCTION YOU SHOULD CALL EXTERNALLY!!!
+		parse(data: string) {
+			this.addDataToBuffer(data);
+
+			while (true) {
+				let msg = this.getLineFromBuffer();
+				if (!msg) break;
+				this.parseMessage(msg);
+			}
+		}
+
+		// Take a parsed message from internal msg queue
+		getMessage() : Message {
+			return this.messages.shift();
+		}
+
+		addDataToBuffer(data: string) {
+			this.buffer += data;
+		}
+
+		stripLeadingRegex(buf: string, regex: RegExp) : string {
+			let match = regex.exec(buf);
+			if (!match) return buf;
+			return buf.slice(match[0].length);
+		}
+
+		// Returns null if no more message
+		getLineFromBuffer() {
+			let pos = 0;
+			let end = 0;
+			let msg = '';
+
+			// Find end of first message. Return if not found (incomplete)
+			do {
+				// Find next newline
+				end = this.buffer.indexOf('\n', pos);
+				if (end < 0) return null; // Incomplete message, return and wait for more data
+				msg = this.buffer.slice(0, end+1);
+
+				// Make sure that newline wasn't in an encap tag
+				do {
+					let enBegin = msg.indexOf(Encap.BEGIN, pos);
+					if (enBegin < 0) break; // If no begin tag in message, we're okay, break
+
+					let enEnd = this.buffer.indexOf(Encap.END, enBegin);
+					if (enEnd < 0) return null; // If begin tag but no matching end tag, incomplete message, return
+					pos = enEnd;
+				} while (pos < end); // If we've gone past end of message, we need to enlarge message to next newline
+			} while (pos > end); // This will be true if and only if newline was inside encap tags
+
+			// Remove message out of buffer
+			this.buffer = this.buffer.slice(end+1);
+			return msg;
+		}
+
+		// Parse single message
+		parseMessage(buf: string) {
+			let msg = null;
+
+			do { // This loop exists only so I can break from it
+				buf = buf.trim();
+
+				// Parse command
+				let match = /^[\S]+/.exec(buf);
+				if (!match) return;
+				try {
+					msg = new Message(match[0]);
+				} catch(e) {
+					console.error(e);
+					return;
+				}
+				buf = buf.slice(match[0].length).trim();
+
+				// Parse object
+				match = /^[\S]+/.exec(buf);
+				if (!match) break;
+				for (let obj of match[0].split('.')) {
+					let objParts = obj.split('#');
+					try {
+						msg.addObj(objParts[0], objParts[1]);
+					} catch(e) {
+						console.error(e);
+						return;
+					}
+				}
+				buf = buf.slice(match[0].length).trim();
+
+				// Parse properties
+				this.parsePropList(msg, buf);
+
+			} while(false);
+
+			// Success, put message on queue
+			if (msg) this.messages.push(msg);
+		}
+
+		parsePropList(msg: Message, buf: string) {
+			// Don't be strict about what we accept here
+			// Parse space or comma separated list of props/sysprops, in any order
+			// This makes parsing easier and results in less code
+			while (true) {
+				buf = this.stripLeadingRegex(buf, /^[\s,]*/);
+				let match = /^\$?[a-z]\w*/.exec(buf);
+				if (!match) return; // End of list
+				let prop = new Prop(match[0]);
+				// Remove name from buffer
+				buf = buf.slice(prop.name.length).trim();
+				// If next non-ws char is '=', property has a value
+				if (buf[0] === '=') {
+					buf = this.parseValue(prop.val, buf.slice(1));
+				}
+
+				msg.addProp(prop);
+			}
+		}
+
+		parseValue(val: Value, buf: string) : string {
+			buf = buf.trim();
+
+			// Array is special case, we can't parse this as a regex
+			if (buf[0] === '[') {
+				buf = buf.slice(1); // Remove '['
+				let arr = [];
+				while (true) {
+					buf = buf.trim();
+
+					let c = buf[0];
+					if (c === ']') {
+						buf = buf.slice(1); // Remove ']' and terminate array
+						break;
+					} else if (c === ',') {
+						buf = buf.slice(1); // Remove ',' and continue to next value
+					} else {
+						let v = new Value();
+						buf = this.parseValue(v, buf);
+						if (v.val === null) {
+							console.error(`Message.parseValue(): Illegal character '${c}' in array`);
+							break;
+						}
+						arr.push(v);
+					}
+				}
+				val.setVal(arr);
+				return buf;
+			}
+
+			// Loop only so we can use 'break'
+			let match;
+			do {
+				// Test for double-quoted string
+				match = /^"((\\.|[^"\\])*)"/.exec(buf);
+				if (match) {
+					val.setVal(match[1]).setType(Type.STRING);
+					break;
+				}
+				// Test for single-quoted string
+				match = /^'((\\.|[^'\\])*)'/.exec(buf);
+				if (match) {
+					val.setVal(match[1]).setType(Type.STRING);
+					break;
+				}
+				// Test for enum
+				match = /^[a-z]\w*/i.exec(buf);
+				if (match) {
+					val.setVal(match[0]).setType(Type.ENUM);
+					break;
+				}
+				// Test for number
+				match = /^[0-9a-fx.+\-]+/i.exec(buf);
+				if (match) {
+					let num = match[0];
+					if (num.indexOf('.') < 0) {
+						val.setVal(parseInt(num));
+					} else {
+						val.setVal(parseFloat(num));
+					}
+					break;
+				}
+				// Test for encap
+				match = RegExp(`^${Encap.BEGIN}(.*)${Encap.END}`).exec(buf);
+				if (match) {
+					val.setVal(match[1]).setType(Type.ENCAP);
+					break;
+				}
+				// Couldn't parse as anything, skip to next whitespace or comma
+				match = /^[^\s,]*/.exec(buf);
+				console.error(`Message.parseVal(): Cannot parse '${match[0]}' into value`);
+			} while (false);
+
+			// We end up here for all cases we use regex for (all except array)
+			// Remove matched value (even if invalid) from buffer
+			// match CANNOT be undefined at this point
+			return buf.slice(match[0].length);
+		}
+	}
+};
